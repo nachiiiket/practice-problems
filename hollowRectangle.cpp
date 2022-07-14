49bcd2ae4ffe9c40d3efa2e2c041faf91ae0a7ca
nachiiiket <yawalkarnachiket@gmail.com>
2022-07-14 20:16:32 +0530

    hollow rectangle

---
+++ hollowRectangle.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int row = 1;
+    while(row <= 1)
+    {
+        int column = 1;
+        while(column <= 1)
+        {
+            cout<<"*";
+            column++;
+        }
+        cout<<endl;
+        row++;
+    }
+ 
+    return 0;
+}
